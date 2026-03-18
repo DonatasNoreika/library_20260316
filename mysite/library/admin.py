a8b09d33875b0e62b95c961d71dd0d6753a1cb70
DonatasNoreika <donoras@gmail.com>
2026-03-18 09:20:51 +0200

    3: +knygų egzempliorių sąraše nustatyti norimi stulpeliai, filtrai, pakeistas fieldsets

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -4,7 +4,17 @@ from .models import Author, Genre, Book, BookInstance
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'isbn', 'display_genre']
 
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'due_back', 'status']
+    list_filter = ['book', 'due_back', 'status']
+
+    fieldsets = [
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    ]
+
 admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
