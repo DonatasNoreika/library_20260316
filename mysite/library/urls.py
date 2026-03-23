4a5d5be5915bf88b71b29187154590702ce0a672
DonatasNoreika <donoras@gmail.com>
2026-03-23 09:27:51 +0200

    5: +paspaudus ant autoriaus, patenkama į jo išsamų aprašymą (DetailView)

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('authors/', views.authors, name='authors'),
+    path('authors/<int:author_pk>/', views.author, name='author'),
 ]
